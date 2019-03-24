272cf8edbca4e4eaa4a908e771309bf1a2cb3f1d
Mark <mark.volfson@gmail.com>
2019-03-23 21:30:19 -0400

    more edits

--- a2.m
+++ a2.m
@@ -9,5 +9,6 @@ img = imread('parliament-right.jpg');
 grayImg1 = rgb2gray(img);
 finImg2 = im2single(grayImg1);
 
+run('vlfeat-0.9.21/toolbox/vl_setup');
 [f1, d1] = vl_sift(finImg1);
 [f2, d2] = vl_sift(finImg2);
