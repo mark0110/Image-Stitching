f4ad8d9c15255ede7cd2183c12585f63bd50eba3
Mark <mark.volfson@gmail.com>
2019-03-27 21:30:32 -0400

    edits

--- a2.m
+++ a2.m
@@ -6,9 +6,11 @@ grayImg1 = rgb2gray(img);
 finImg1 = im2single(grayImg1);
 
 img = imread('parliament-right.jpg');
-grayImg1 = rgb2gray(img);
-finImg2 = im2single(grayImg1);
+grayImg2 = rgb2gray(img);
+finImg2 = im2single(grayImg2);
 
-run('vlfeat-0.9.21/toolbox/vl_setup');
+%run('vlfeat-0.9.21/toolbox/vl_setup');
 [f1, d1] = vl_sift(finImg1);
 [f2, d2] = vl_sift(finImg2);
+
+[matches, scores] = vl_ubcmatch(d1, d2);
--- a2.py
+++ a2.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+import cv2 as cv
+import numpy as np
+
+img = cv.imread("city.jpg")
+img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+city_ft = np.fft.fft2(city_img)
