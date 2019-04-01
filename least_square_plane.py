7d858959388f0ece5c70c56967728cf451dc2037
Mike <m3teitel@ryerson.ca>
2019-04-01 15:26:20 -0400

    executables

--- least_square_plane.py
+++ least_square_plane.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as mplt
+from mpl_toolkits.mplot3d import Axes3D
 
 def lsp():
     var = np.array([[3, 4, 6]])
@@ -10,10 +12,15 @@ def lsp():
     A = np.transpose(A)
     b = np.matmul(A, var)
     b = np.transpose(b)
-
+    X, Y = np.meshgrid(x, y)
+    Z = (var[0]*X + var[1]*Y + var[2])
     noise = np.random.normal(0, 1, 500)
-
-
+    Znoise = np.matmul(noise, np.transpose(noise))
+    Z = Z + Znoise
+    fig = mplt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(X, Y, Z)
+    mplt.show()
     gaussianb = b + noise
     p = np.linalg.pinv(A)
     gaussianb = np.transpose(gaussianb)
